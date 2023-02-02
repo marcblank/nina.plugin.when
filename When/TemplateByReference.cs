4db857ea4507f26760253b58bf0f4f623aaf66c0
Marc Blank <marc.blank@live.com>
2023-02-02 12:28:41 -0800

    TBR good!

--- When/TemplateByReference.cs
+++ When/TemplateByReference.cs
@@ -1,142 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//using Newtonsoft.Json;
-//using NINA.Core.Model;
-//using NINA.Sequencer.Container;
-//using NINA.Sequencer.SequenceItem;
-//using NINA.Sequencer.Validations;
-//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel.Composition;
-//using System.Text.RegularExpressions;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using System.Windows.Media;
-//using NCalc;
-//using Castle.Core.Internal;
-//using NINA.Core.Utility.Notification;
-//using System.Linq;
-//using System.Text;
-//using Accord.IO;
-//using Namotion.Reflection;
-//using NINA.Sequencer.DragDrop;
-//using NINA.Sequencer.Trigger;
-//using System.Windows.Input;
-//using NINA.Equipment.Interfaces.Mediator;
-//using NINA.Equipment.Equipment.MySafetyMonitor;
-//using NINA.Core.Utility;
-//using NINA.Sequencer.Conditions;
-//using NINA.Sequencer.Interfaces.Mediator;
-//using NINA.ViewModel.Sequencer;
-//using System.Reflection;
-//using NINA.Sequencer;
-
-//namespace WhenPlugin.When {
-//    [ExportMetadata("Name", "Template by Reference")]
-//    [ExportMetadata("Description", "")]
-//    [ExportMetadata("Icon", "Pen_NoFill_SVG")]
-//    [ExportMetadata("Category", "When (and If)")]
-//    [Export(typeof(ISequenceItem))]
-//    [JsonObject(MemberSerialization.OptIn)]
-//    public class TemplateByReference : IfCommand, IValidatable {
-
-//        protected ISafetyMonitorMediator safetyMediator;
-//        protected ISequenceMediator sequenceMediator;
-//        protected ISequenceNavigationVM sequenceNavigationVM;
-//        protected TemplateController templateController;
-
-
-//        [ImportingConstructor]
-//        public TemplateByReference (ISafetyMonitorMediator safetyMediator, ISequenceMediator sequenceMediator) {
-//            this.safetyMediator = safetyMediator;
-//            this.sequenceMediator = sequenceMediator;
-//            //IsExpanded = false;
-
-//            Instructions = new SequentialContainer();
-//            Instructions.AttachNewParent(Parent);
-
-//            TemplateName = "";
-
-//            // GetField() returns null, so iterate?
-//            var fields = sequenceMediator.GetType().GetRuntimeFields();
-//            foreach (FieldInfo fi in fields) {
-//                if (fi.Name.Equals("sequenceNavigation")) {
-//                    sequenceNavigationVM = (ISequenceNavigationVM)fi.GetValue(sequenceMediator);
-//                    ISequence2VM s2vm = sequenceNavigationVM.Sequence2VM;
-//                    if (s2vm != null) {
-//                        PropertyInfo pi = s2vm.GetType().GetRuntimeProperty("TemplateController");
-//                        templateController = (TemplateController)pi.GetValue(s2vm);
-//                     }
-//                }
-//            }
-//        }
-
-//        public TemplateByReference(TemplateByReference copyMe) : this(copyMe.safetyMediator, copyMe.sequenceMediator) {
-//            if (copyMe != null) {
-//                CopyMetaData(copyMe);
-//                Instructions = (SequentialContainer)copyMe.Instructions.Clone();
-//            }
-//        }
-
-//        // WE DO NOT WANT TO SERIALIZE THIS
-//        public SequentialContainer Instructions;
-
-//        public override object Clone() {
-//            return new TemplateByReference(this) {
-//            };
-//        }
-
-//        private string templateName = "";
-
-//        [JsonProperty]
-//        public string TemplateName {
-//            get => templateName;
-//            set {
-//                templateName = value;
-//                if (Instructions.Items.IsNullOrEmpty()) {
-//                } else {
-//                    Instructions.Items.Clear();
-//                }
-//                RaisePropertyChanged();
-//                Validate();
-//            }
-//        }
-
-//        public new bool Validate() {
-//            if (templateController == null) return true;
-//            var i = new List<string>();
-//            if (Instructions.Items.IsNullOrEmpty()) {
-//                foreach (TemplatedSequenceContainer tsc in templateController.Templates) {
-//                    if (tsc.Container.Name.Equals(TemplateName)) {
-//                        Console.WriteLine("Found template!");
-//                        Instructions.Items.Add((ISequenceContainer)tsc.Container.Clone());
-//                        foreach (ISequenceItem item in Instructions.Items) {
-//                            item.AttachNewParent(Instructions);
-//                        }
-//                        RaisePropertyChanged("Instructions");
-//                        return true;
-//                    }
-//                }
-//                i.Add("Template not found: " + TemplateName);
-//            }
-//            Issues = i;
-//            return i.Count == 0;
-//        }
-
-//        public override string ToString() {
-//            return $"Category: {Category}, Item: {Name}";
-//        }
-
-//        public override Task Execute(IProgress<ApplicationStatus> progress, CancellationToken token) {
-//            throw new NotImplementedException();
-//        }
-//    }
-//}
-
+﻿
 using Newtonsoft.Json;
 using NINA.Core.Model;
 using NINA.Sequencer.SequenceItem;
@@ -157,11 +19,12 @@ using NINA.Sequencer;
 using System.Runtime.CompilerServices;
 using NINA.Sequencer.Container;
 using Castle.Core.Internal;
+using NINA.Core.Utility;
 
 namespace WhenPlugin.When {
     [ExportMetadata("Name", "Template by Reference")]
     [ExportMetadata("Description", "Executes an instruction set if the safety monitor indicates that conditions are safe.")]
-    [ExportMetadata("Icon", "ShieldSVG")]
+    [ExportMetadata("Icon", "BoxClosedSVG")]
     [ExportMetadata("Category", "When (and If)")]
     [Export(typeof(ISequenceItem))]
     [JsonObject(MemberSerialization.OptIn)]
@@ -200,7 +63,6 @@ namespace WhenPlugin.When {
         public IfContainer Instructions { get; protected set; }
 
         public IList<string> issues = new List<string>();
-
         public IList<string> Issues {
             get => issues;
             set {
@@ -209,22 +71,41 @@ namespace WhenPlugin.When {
             }
         }
 
-        public override object Clone() {
-            TemplateByReference clone = new TemplateByReference(this);
-            clone.TempName= this.TempName;
-            return clone;
-        }
-
-        public string tempName;
-
         [JsonProperty]
-        public string TempName {
-            get { return tempName; }
+        public string TemplateName { get; set; }
+
+        public IList<TemplatedSequenceContainer> Templates { get => templateController.Templates; }
+
+        private TemplatedSequenceContainer selectedTemplate;
+        public TemplatedSequenceContainer SelectedTemplate {
+            get => selectedTemplate;
             set {
-                tempName = value;
-                RaisePropertyChanged();
+                selectedTemplate = value;
+                if (Instructions.Items.Count > 0) {
+                    Instructions.Items.Clear();
+                }
+                TemplateName = selectedTemplate.Container.Name;
+                Instructions.Items.Add((ISequenceContainer)SelectedTemplate.Container.Clone());
+                foreach (ISequenceItem item in Instructions.Items) {
+                    item.AttachNewParent(Instructions);
+                }
+                RaisePropertyChanged("Instructions");
                 Validate();
             }
+        }
+
+        public override object Clone() {
+            TemplateByReference clone = new TemplateByReference(this);
+            clone.TemplateName = TemplateName;
+            if (TemplateName != null && templateController != null) {
+                foreach (var tmp in templateController.Templates) {
+                    if (tmp.Container.Name.Equals(TemplateName)) {
+                        Logger.Info("Cloning TemplateByReference; found template " + TemplateName);
+                        clone.SelectedTemplate = tmp;
+                    }
+                }
+            }
+            return clone;
         }
 
         public async override Task Execute(IProgress<ApplicationStatus> progress, CancellationToken token) {
@@ -232,25 +113,19 @@ namespace WhenPlugin.When {
             await runner.RunConditional();
         }
 
-        public new bool Validate() {
+        public bool Validate() {
             if (templateController == null) return true;
+            
             var i = new List<string>();
-            if (TempName.IsNullOrEmpty()) {
-                i.Add("A template name must be specified!");
-            } else if (Instructions.Items.Count == 0) {
-                foreach (TemplatedSequenceContainer tsc in templateController.Templates) {
-                    if (tsc.Container.Name.Equals(TempName)) {
-                        Console.WriteLine("Found template!");
-                        Instructions.Items.Add((ISequenceContainer)tsc.Container.Clone());
-                        foreach (ISequenceItem item in Instructions.Items) {
-                            item.AttachNewParent(Instructions);
-                        }
-                        RaisePropertyChanged("Instructions");
-                        return true;
-                    }
+
+            if (SelectedTemplate == null) {
+                if (TemplateName == null) {
+                    i.Add("A template must be selected!");
+                } else {
+                    i.Add("The specified template '" + TemplateName + "' was not found.");
                 }
-                i.Add("The template '" + TempName + "' was not found in your template list.");
             }
+
             Issues = i;
             return i.Count == 0;
         }
