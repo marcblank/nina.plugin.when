924f25485f100497e7c00ee4f621cd11652a00e8
Marc Blank <marc.blank@live.com>
2023-12-20 10:49:15 -0800

    Remove not defined error

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("3.11.0.10")]
-[assembly: AssemblyFileVersion("3.11.0.10")]
+[assembly: AssemblyVersion("3.11.0.12")]
+[assembly: AssemblyFileVersion("3.11.0.12")]
 
 // [MANDATORY] The name of your plugingit st
 [assembly: AssemblyTitle("Sequencer Powerups")]
