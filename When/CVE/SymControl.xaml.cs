bf722c9dc5a8254e985082644865a6b2cb264034
Marc Blank <marc.blank@live.com>
2023-12-27 14:27:04 -0800

    Major move of files to subdirectories

---
+++ When/CVE/SymControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using NINA.Sequencer;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WhenPlugin.When {
+
+    public partial class SymControl : UserControl {
+        public SymControl() {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ExpProperty =
+             DependencyProperty.Register("Exp", typeof(Expr), typeof(SymControl), null);
+
+        public Expr Exp { get; set; }
+
+        public void ShowConstants(object sender, ToolTipEventArgs e) {
+            //TextBox tb = (TextBox)sender;
+            //ISequenceEntity item = (ISequenceEntity)tb.DataContext;
+            //var stack = ConstantExpression.GetKeyStack(item);
+            //if (stack == null || stack.Count == 0) {
+            //    tb.ToolTip = "There are no valid, defined constants.";
+            //} else {
+            //    tb.ToolTip = ConstantExpression.DissectExpression(item, tb.Text, stack);
+            //}
+        }
+        public void IfConstant_PredicateToolTip(object sender, ToolTipEventArgs e) {
+        //    TextBox predicateText = (TextBox)sender;
+        //    IfConstant ifConstant = (IfConstant)(predicateText.DataContext);
+        //    predicateText.ToolTip = ifConstant.ShowCurrentInfo();
+        //
+        //
+        }
+
+
+    }
+}
+
