f6341e59fef8e32e5ef084e038f7750396c1d374
Marc Blank <marc.blank@live.com>
2024-07-01 16:29:00 -0700

    Builds and runs

---
+++ When/Dockable/DockableTemplates.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WhenPlugin.When {
+    /// <summary>
+    /// Interaction logic for DockableTemplates.xaml
+    ///
+    /// </summary>
+    /// 
+    [Export(typeof(ResourceDictionary))]
+    public partial class DockableTemplates : ResourceDictionary {
+        public DockableTemplates() {
+            InitializeComponent();
+        }
+    }
+}
