1ed9ce8ecc896fdfeabcfde63f926491fb6449a6
Marc Blank <marc.blank@live.com>
2024-07-17 11:24:20 -0700

    Powerups 3.14.8.7

--- When/CVE/IfContainer.cs
+++ When/CVE/IfContainer.cs
@@ -52,27 +52,23 @@ namespace WhenPlugin.When {
                 if (PseudoParent is IDSOTargetProxy w && w.DSOProxyTarget() != null) {
                     return w.DSOProxyTarget();
                 }
-                
+
                 ISequenceContainer parent = PseudoParent as ISequenceContainer;
-                while (parent != null) {
-                    if (parent is IDeepSkyObjectContainer dso) {
-                        return dso.Target;
-                    }
-                    parent = parent.Parent;
-                }
-            
-                IProfileService profileService = WhenPlugin.ProfileService;
-                InputTarget t = new InputTarget(Angle.ByDegree(profileService.ActiveProfile.AstrometrySettings.Latitude), Angle.ByDegree(profileService.ActiveProfile.AstrometrySettings.Longitude), profileService.ActiveProfile.AstrometrySettings.Horizon);
-                if (Parent != null) {
-                    ContextCoordinates cc = ItemUtility.RetrieveContextCoordinates(Parent);
+
+                if (parent != null) {
+                    IProfileService profileService = WhenPlugin.ProfileService;
+                    ContextCoordinates cc = ItemUtility.RetrieveContextCoordinates(parent);
                     if (cc != null) {
+                        InputTarget t = new InputTarget(Angle.ByDegree(profileService.ActiveProfile.AstrometrySettings.Latitude), Angle.ByDegree(profileService.ActiveProfile.AstrometrySettings.Longitude), profileService.ActiveProfile.AstrometrySettings.Horizon);
                         t.InputCoordinates.Coordinates = cc.Coordinates;
+                        return t;
                     }
                 }
-                return t;
+                return null;
             }
             set { }
         }
+
         public NighttimeData NighttimeData => throw new NotImplementedException();
         public override void ResetProgress() {
             base.ResetProgress();
