b52de5c3183d641a79344f4d61bea7a4f0bb533c
Marc Blank <marc.blank@live.com>
2024-07-16 17:44:27 -0700

    Fix IfContainer for  no target

--- When/CVE/IfContainer.cs
+++ When/CVE/IfContainer.cs
@@ -54,22 +54,17 @@ namespace WhenPlugin.When {
                 }
                 
                 ISequenceContainer parent = PseudoParent as ISequenceContainer;
-                while (parent != null) {
-                    if (parent is IDeepSkyObjectContainer dso) {
-                        return dso.Target;
-                    }
-                    parent = parent.Parent;
-                }
-            
-                IProfileService profileService = WhenPlugin.ProfileService;
-                InputTarget t = new InputTarget(Angle.ByDegree(profileService.ActiveProfile.AstrometrySettings.Latitude), Angle.ByDegree(profileService.ActiveProfile.AstrometrySettings.Longitude), profileService.ActiveProfile.AstrometrySettings.Horizon);
-                if (Parent != null) {
-                    ContextCoordinates cc = ItemUtility.RetrieveContextCoordinates(Parent);
+
+                if (parent != null) {
+                    IProfileService profileService = WhenPlugin.ProfileService;
+                    ContextCoordinates cc = ItemUtility.RetrieveContextCoordinates(parent);
                     if (cc != null) {
+                        InputTarget t = new InputTarget(Angle.ByDegree(profileService.ActiveProfile.AstrometrySettings.Latitude), Angle.ByDegree(profileService.ActiveProfile.AstrometrySettings.Longitude), profileService.ActiveProfile.AstrometrySettings.Horizon);
                         t.InputCoordinates.Coordinates = cc.Coordinates;
+                        return t;
                     }
                 }
-                return t;
+                return null;
             }
             set { }
         }
