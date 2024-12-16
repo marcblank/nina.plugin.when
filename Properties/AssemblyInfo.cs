3c6d0426e51e02f5f9b09fb5869b0b7d5befc33d
Marc VBlank <marc.blank@live.com>
2024-12-16 14:08:08 -0800

    Powerups Beta 3.21.2.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 //Should be incremented for each new release build of a plugin
 
 // Odd minor releases for Beta
-[assembly: AssemblyVersion("3.21.2.4")]
-[assembly: AssemblyFileVersion("3.21.2.4")]
+[assembly: AssemblyVersion("3.21.2.5")]
+[assembly: AssemblyFileVersion("3.21.2.5")]
 
 // [MANDATORY] The name of your plugingit st
 [assembly: AssemblyTitle("Sequencer Powerups")]
